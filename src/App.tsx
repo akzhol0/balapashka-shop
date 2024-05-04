83bd02f35195077dee7d208ed6531fc397471278
Akzhol <ak.akzhol1603@gmail.com>
2024-05-04 19:18:29 +0500

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React, { useContext } from "react";
-import "./global.scss";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import Catalog from "./components/catalog/Catalog";
 import UnknownPage from "./components/UnknownPage";
 import { ContextOverAll } from "./context/logic";
 import SingleItem from "./components/catalog/SingleItem";
+import "./global.scss";
 import Login from "./auth/Login";
 import Register from "./auth/Register";
-import { contextData } from "./context/logic";
 
 function App() {
-  const { userIn } = useContext(contextData);
-
   return (
     <ContextOverAll>
       <BrowserRouter>
@@ -20,8 +17,8 @@ function App() {
           <Route path="/catalog" element={<Catalog />}></Route>
           <Route path="/catalog/:category/:id" element={<SingleItem />}></Route>
           <Route path="/" element={<HomePage />}></Route>
-          <Route path="/login" element={<Login />}></Route>
           <Route path="/register" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
           <Route path="*" element={<UnknownPage />}></Route>
         </Routes>
       </BrowserRouter>
